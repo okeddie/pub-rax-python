e9fd21227527863b3077647285d84a88c1c28887
eddie.vasquez <eddie.vasquez@rackspace.com>
2015-02-05 21:17:52 -0600

    adding serial option

--- get_verb.py
+++ get_verb.py
@@ -1,8 +1,11 @@
 from optparse import OptionParser
 parser = OptionParser()
 parser.add_option('-v', '--verbose', dest="verbose", default=False, action="store_true")
-(options, args) = parser.parse_args()
-if options.verbose:
+parser.add_option('-s', '--serial-numbers', dest="serial_num", default=False, action="store_true")
+(opts, args) = parser.parse_args()
+if opts.verbose:
     print "verbose is ON"
 else:
     print "verbose is OFF"
+if opts.serial_num:
+    print "some sn here"
