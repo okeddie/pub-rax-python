950e8b1a83c19f2597000d9eeb6b55804d600268
eddie vasquez <eddie.p.vasquez@gmail.com>
2018-09-25 21:47:52 -0500

    adding docs for lab09.py.

--- lab09.py
+++ lab09.py
@@ -12,15 +12,20 @@ generated_num = randint(1,101)
 attempts = 1
 
 while True:
-    # initialize input
+    # set input
     user_guess = input("Enter your guess: ")
+    # test if user guessed the correct number.
     if int(user_guess) == generated_num:
         break
+    # test if the user guessed too high.
     if int(user_guess) > generated_num:
         print("Your guess is too high")
+    # test if the user guessed too low.
     if int(user_guess) < generated_num:
         print("Your guess is too low")
+    # auto increment attempts.
     attempts += 1
 
+# return feedback when guessed correct.
 print("Your guess is correct!")
 print("You succeeded in {0:d}".format(attempts),"attempts")
