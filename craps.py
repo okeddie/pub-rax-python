e64d4f75f90aae5c33c02a305551fd7a85dad74b
Eddie Vasquez <eddie.p.vasquez@gmail.com>
2018-09-27 14:41:49 -0500

    fixing tabs/spaces.

--- craps.py
+++ craps.py
@@ -12,8 +12,6 @@ literal_rolls = 0
 roll_2_percent = 0
 roll_7_percent = 0
 point_rolls = 0
-wins = 0
-loses = 0
 
 def roll_die():
 	    roll_dice1 = randrange(1,7)
@@ -22,21 +20,21 @@ def roll_die():
 	    return roll_total
 
 for i in range(1,roll_count,1):
-	roll_total = roll_die()
-	try:
-	    roll_total = int(roll_total)
-	except ValueError:
-		print("failed to set roll total count")
-		break
-	literal_rolls += 1
-	if roll_total == 2:
-		roll_2_count += 1
-	if roll_total == 7:
-		roll_7_count += 1
-	if literal_rolls == 1:
-		if roll_total == 7 or roll_total == 11:
-                    print("you rolled a {0:d} on your {1:d}st roll. You WIN!".format(roll_total, literal_rolls))
-	            break
+        roll_total = roll_die()
+        try:
+            roll_total = int(roll_total)
+        except ValueError:
+            print("failed to set roll total count")
+            break
+        literal_rolls += 1
+        if roll_total == 2:
+            roll_2_count += 1
+        if roll_total == 7:
+            roll_7_count += 1
+        if literal_rolls == 1:
+            if roll_total == 7 or roll_total == 11:
+                print("you rolled a {0:d} on your {1:d}st roll. You WIN!".format(roll_total, literal_rolls))
+	        break
 		if roll_total == 2 or roll_total == 3 or roll_total == 12:	        
                     print("you rolled a {0:d} on your {1:d}st roll. You LOST!".format(roll_total, literal_rolls))
                     break
